30ec518476b2c4a62d0e446919f295357371727f
Ashutosh-tornado <ashutoshjr.77@gmail.com>
2026-03-28 12:40:20 +0530

    updated api url

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -8,7 +8,7 @@ const handleSubmit = async (e) => {
   };
 
   try {
-    const res = await fetch("http://localhost:5000/contact", {
+    const res = await fetch("https://stackminds-agency.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
